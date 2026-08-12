4647145083e3b61971e0b6e760a12da05a34e4fb
Christian Mueth <christianelijahmueth@gmail.com>
2026-08-12 07:07:22 -0400

    Show newest Lego comics first

--- app/lego-comics/page.tsx
+++ app/lego-comics/page.tsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 import Link from "next/link";
 import comics from "./content.json";
 
+const comicsNewestFirst = [...comics].reverse();
+
 export default function LegoComicIndex() {
   return (
     <main className="shell">
@@ -15,7 +17,7 @@ export default function LegoComicIndex() {
       </section>
 
       <section className="comic-grid" aria-label="Old Lego Comics">
-        {comics.map((comic) => (
+        {comicsNewestFirst.map((comic) => (
           <Link key={comic.number} href={`/lego-comics/${comic.number}`} className="comic-card">
             <Image
               src={comic.type === "video" ? comic.poster ?? comic.src : comic.src}
